50dbded4abd25b139886e673f38f12ccf5e0c2cf
Jenny Zhang <jenny.zhang@procuraduria.gob.pa>
2024-07-26 09:37:40 -0500

    [added]configuración, librería DaisyUI y estructura de los componentes

---
+++ src/modules/projects/layouts/ProjectLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Top Menu -->
+
+    <top-menu />
+    <main>
+      <!-- Side Menu -->
+      <side-menu />
+
+      <!-- RouterView -->
+
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import ProjectLayout from '@/modules/projects/layouts/ProjectLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectLayout,
+    },
+  ],
+});
+
+export default router;
