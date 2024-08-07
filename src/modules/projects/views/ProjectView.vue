1fc978291b169d019cd0f70dc2b01ce6b30a3ab5
Jenny Zhang <jenny.zhang@procuraduria.gob.pa>
2024-08-07 14:31:40 -0500

    [update]Tarea: Agregar tareas al proyecto

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,18 +1,59 @@
 <template>
-  <bread-crumbs :name="project?.name ?? 'No name'" />
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No name'" />
+    </section>
+
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completadas</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+            </tr>
+
+            <tr>
+              <th></th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Nueva tarea"
+                />
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
 import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectsStore } from '../store/projects.store';
-import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+
 interface Props {
   id: string;
 }
 
+const router = useRouter();
 const props = defineProps<Props>();
-
 const projectStore = useProjectsStore();
 const project = ref<Project | null>();
 
@@ -22,6 +63,9 @@ watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   {
     immediate: true,
